683b7ce71d92e1eb2ad1c6e8c1cb07550a516c65
unknown <forkan.saadaoui@gmail.com>
2024-11-12 16:05:09 +0100

    project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Tajawal  } from 'next/font/google';
+import "./globals.css";
+
+const tajawal = Tajawal ({
+  subsets: ['arabic'],
+  weight: ['300', '400', '500', '700'],
+});
+
+export const metadata: Metadata = {
+  title: "Learn English",
+  description: "Learn English By Using AI From OpenAi",
+  authors: [
+    {
+      name: "Saadaoui Mahmoud",
+    },
+  ],
+  icons: {
+    icon: "/icon.png",
+  },
+};
+
+type Props = {
+  children: React.ReactNode
+}
+
+export default function RootLayout({children}: Props) {
+  return (
+    <html lang="en">
+      <body className={tajawal.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
